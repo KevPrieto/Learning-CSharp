7df4f2431d3e2464d4667486c10fd9e68ac8f22f
KevPrS <kgps1003@gmail.com>
2025-09-29 13:04:49 +0200

    Added files inside Fundamentals folder

---
+++ Fundamentals/Algebra/Algebra.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Algebra
+{
+    class Algebra
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your name here: ");
+            string myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name here: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you! " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fundamentals/Hello World/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// I'll use classic C# style for this one, as I'm learning the language.
+using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First time using C# in Visual Studio!");
+            Console.ReadLine();
+        }
+    };
+}   
+
